7e16c6e308b5f359e389f1a2b7be043ef89f56a4
haojietyap <haojietyapp@gmail.com>
2025-10-17 15:27:52 +0800

    Rename parameters in AddConfirmedJobController

--- Controllers/ConfirmedJobs/AddConfirmedJobController.php
+++ Controllers/ConfirmedJobs/AddConfirmedJobController.php
@@ -2,8 +2,9 @@
 require_once __DIR__ . '/../../Entities/ConfirmedJob.php';
 
 class AddConfirmedJobController {
-    public function add($jobId, $cleanerId, $homeownerId, $matchedDate) {
+    public function add($jobId, $CSRId, $PINId, $matchedDate) {
         $confirmedJob = new ConfirmedJob();
-        return $confirmedJob->addConfirmedJob($jobId, $cleanerId, $homeownerId, $matchedDate);
+        return $confirmedJob->addConfirmedJob($jobId, $CSRId, $PINId, $matchedDate);
     }
 }
+
