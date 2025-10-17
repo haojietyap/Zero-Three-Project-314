33aaa3a00559c0f0ed9906e538f8e94abc6dc3de
haojietyap <haojietyapp@gmail.com>
2025-10-17 15:32:54 +0800

    Update CheckFavoriteStatusController.php

--- Controllers/Homeowner/CheckFavoriteStatusController.php
+++ Controllers/Homeowner/CheckFavoriteStatusController.php
@@ -2,9 +2,10 @@
 require_once __DIR__ . '/../../Entities/Favorite.php';
 
 class CheckFavoriteStatusController {
-    public function isFavorited($homeownerId, $cleanerId) {
+    public function isFavorited($PINId, $CSRId) {
         $favorite = new Favorite();
-        return $favorite->isFavorited($homeownerId, $cleanerId);
+        return $favorite->isFavorited($PINId, $CSRId);
     }
 }
+
 
