18d189d66db65e8eca97504fa358b314227cf464
haojietyap <haojietyapp@gmail.com>
2025-10-17 15:56:30 +0800

    Update and rename CreateManagerProfileController.php to CreateManagementProfileController.php

--- Controllers/Profiles/Manager/CreateManagerProfileController.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../../../Entities/ManagerProfile.php';
-
-class CreateManagerProfileController {
-    public function create($userId, $phone, $address) {
-        $profile = new ManagerProfile();
-        return $profile->create($userId, $phone, $address);
-    }
-}
