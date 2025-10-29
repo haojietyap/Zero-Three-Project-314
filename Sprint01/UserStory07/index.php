3e242eb9ae8c9150767f38c16d3ec95b104b9bb4
Andre <126609840+youngbasedred@users.noreply.github.com>
2025-10-29 17:32:58 +0800

    Delete Sprint01 directory

--- Sprint01/UserStory07/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-header('Location: ./Controller/ViewProfilesController.php');
-exit;
--- Sprint01/UserStory12/Controller/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-session_start();
-$_SESSION = [];
-session_destroy();
-header('Location: ../Boundary/login.php?msg=Logged+out');
-exit;
