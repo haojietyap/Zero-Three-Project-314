eb8828f1e60120c6a6bbd8778b92f895b1419bb2
haojietyap <haojietyapp@gmail.com>
2025-11-08 13:45:09 +0800

    Delete Sprint03 directory

--- Sprint03/UserStory38/Controller/DailyReportController.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../../Entities/DailyReport.php';
-
-class DailyReportController {
-    public function getTodayCount() {
-        $confirmedJob = new ConfirmedJob();
-        return $confirmedJob->countConfirmedToday();
-    }
-}
--- Sprint03/UserStory40/Controller/WeeklyReportController.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../../Entities/WeeklyReport.php';
-
-class WeeklyReportController {
-    public function getStats() {
-        $confirmedJob = new ConfirmedJob();
-        return $confirmedJob->getWeeklyCategoryStats();
-    }
-}
