353a9fbccecc6b97d9289c5dd927a050ed34b89b
sohxinyong-star <sohxinyong@gmail.com>
2025-11-11 00:29:31 +0800

    Delete Sprint02/UserStory17 directory

--- Sprint02/UserStory17/Controller/searchmyrequestscontroller.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/../Entity/Request.php';
-
-class searchMyRequestsController {
-    public function search(int $userID, array $criteria): array {
-        return Request::searchByUser($userID, $criteria);
-    }
-}
